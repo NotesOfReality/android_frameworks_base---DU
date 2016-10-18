06f3ea95991facd58b9a400d1a8f40de1a283c3d
mar-v-in <github@rvin.mooo.com>
2016-10-18 21:51:21 +0800

    PM: Signature spoofing Master Toggle [1/2]
    * @neobuddy89: Moved to Global.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -11614,6 +11614,15 @@ public final class Settings {
                 "storage_settings_clobber_threshold";
 
         /**
+        /**
+         * Whether applications can fake a signature.
+         * 1 = permit apps to fake signature
+         * 0 = disable this feature
+         * @hide
+         */
+        public static final String ALLOW_SIGNATURE_FAKE = "allow_signature_fake";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
