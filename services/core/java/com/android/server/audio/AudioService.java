d5c788406c1df513debc87c05fb157605121a084
maxwen <max.weninger@gmail.com>
2017-09-20 20:54:41 +0800

    [1/2] base: unlink notification volume is back -delete old
    unused volume images -add new one for notification volumes maybe some day one
     will be able to create a vector drawable for this - I am not :)
    
    base: add vector for notification volume mute
    
    thanks to Marko Man

--- services/core/java/com/android/server/audio/AudioService.java
+++ services/core/java/com/android/server/audio/AudioService.java
@@ -672,6 +672,8 @@ public class AudioService extends IAudioService.Stub
 
     private boolean mVisualizerLocked;
 
+    private boolean mLinkNotificationWithVolume;
+
     ///////////////////////////////////////////////////////////////////////////
     // Construction
     ///////////////////////////////////////////////////////////////////////////
@@ -755,6 +757,9 @@ public class AudioService extends IAudioService.Stub
 
         mUseFixedVolume = mContext.getResources().getBoolean(
                 com.android.internal.R.bool.config_useFixedVolume);
+
+        mLinkNotificationWithVolume = Settings.System.getIntForUser(mContentResolver,
+                Settings.System.VOLUME_LINK_NOTIFICATION, 1, UserHandle.USER_CURRENT) == 1;
 
         // must be called before readPersistedSettings() which needs a valid mStreamVolumeAlias[]
         // array initialized by updateStreamVolumeAlias()
@@ -1135,6 +1140,12 @@ public class AudioService extends IAudioService.Stub
             } else {
                 mRingerModeAffectedStreams |= (1 << AudioSystem.STREAM_DTMF);
             }
+        }
+
+        if (mLinkNotificationWithVolume) {
+            mStreamVolumeAlias[AudioSystem.STREAM_NOTIFICATION] = AudioSystem.STREAM_RING;
+        } else {
+            mStreamVolumeAlias[AudioSystem.STREAM_NOTIFICATION] = AudioSystem.STREAM_NOTIFICATION;
         }
 
         mStreamVolumeAlias[AudioSystem.STREAM_DTMF] = dtmfStreamAlias;
@@ -5191,6 +5202,8 @@ public class AudioService extends IAudioService.Stub
                     Settings.System.VOLUME_KEYS_CONTROL_MEDIA_STREAM), false, this);
             mContentResolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.HEADSET_CONNECT_PLAYER), false, this);
+            mContentResolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.VOLUME_LINK_NOTIFICATION), false, this);
         }
 
         @Override
@@ -5213,6 +5226,14 @@ public class AudioService extends IAudioService.Stub
                 updateEncodedSurroundOutput();
 
                 setVolumeKeysControlMediaStream();
+
+                mLinkNotificationWithVolume = Settings.System.getIntForUser(mContentResolver,
+                        Settings.System.VOLUME_LINK_NOTIFICATION, 1, UserHandle.USER_CURRENT) == 1;
+                if (mLinkNotificationWithVolume) {
+                    mStreamVolumeAlias[AudioSystem.STREAM_NOTIFICATION] = AudioSystem.STREAM_RING;
+                } else {
+                    mStreamVolumeAlias[AudioSystem.STREAM_NOTIFICATION] = AudioSystem.STREAM_NOTIFICATION;
+                }
             }
             mLaunchPlayer = Settings.System.getIntForUser(mContext.getContentResolver(),
                     Settings.System.HEADSET_CONNECT_PLAYER, 0, UserHandle.USER_CURRENT);
