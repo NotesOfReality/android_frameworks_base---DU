4a09b336e0b926871fc064332718a0bc5f92ce8e
maxwen <max.weninger@gmail.com>
2018-01-25 15:03:47 +0100

    SystemUI: persist screenrecord quality level
    save state set in qs tile into settings
    
    Change-Id: I708b324a22c3e605fa6411c1a9e1de28b584938b
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: Joe Maples <joe@frap129.org>
    Signed-off-by: LakshyaTaneja <taneja.lakshay5@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4807,6 +4807,11 @@ public final class Settings {
         public static final String SHOW_USB_MODE_DIALOG = "show_usb_mode_dialog";
 
         /**
+         * @hide
+         */
+        public static final String SCREENRECORD_QUALITY_MODE = "screenrecord_quality_mode";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
