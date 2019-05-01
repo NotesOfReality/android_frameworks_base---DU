06b57bfef25734df2ee3e3f50d2793a80b5697a9
NotesOfReality <michelecurcipio@gmail.com>
2019-05-01 21:29:59 +0200

    AdbOverNetworkTile : use the correct MetricsEvent value

--- packages/SystemUI/src/com/android/systemui/qs/tiles/AdbOverNetworkTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/AdbOverNetworkTile.java
@@ -62,7 +62,7 @@ public class AdbOverNetworkTile extends QSTileImpl<BooleanState> {
 
     @Override
     public int getMetricsCategory() {
-        return MetricsEvent.DIRTYTWEAKS;
+        return MetricsEvent.CUSTOM_QUICK_TILES;
     }
 
     @Override
